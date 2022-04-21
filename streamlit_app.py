009e741e415a4de89a9239271c22d1a4b8c42524
JainNoopur <90665884+JainNoopur@users.noreply.github.com>
2022-04-21 14:25:29 +0800

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Oatmeal Cereal')
+streamlit.text('Kale Juice')
+streamlit.text('Eggs and toastie')
+streamlit.text('Mushroom toastie')
